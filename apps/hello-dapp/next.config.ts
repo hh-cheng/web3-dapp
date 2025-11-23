08f5c10701ba15f9f49cc9f28d34d7a7079e47fe
kizuner-bonely <1003306162@qq.com>
2025-11-23 15:05:18 +0800

    chore: update build config

--- apps/hello-dapp/next.config.ts
+++ apps/hello-dapp/next.config.ts
@@ -1,3 +1,4 @@
+import path from 'node:path'
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
@@ -6,6 +7,9 @@ const nextConfig: NextConfig = {
     LOGGER_CONTRACT: process.env.LOGGER_CONTRACT!,
     RED_PACKET_CONTRACT: process.env.RED_PACKET_CONTRACT!,
     WALLETCONNECT_PROJECT_ID: process.env.WALLETCONNECT_PROJECT_ID!,
+  },
+  turbopack: {
+    root: path.join(__dirname), // __dirname 即 apps/hello-dapp
   },
 }
 
